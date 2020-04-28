4303dfccb98fe7976de695968fb88b48419f430f
satanromeo <39999154+satanromeo@users.noreply.github.com>
2020-04-28 18:37:05 +0400

    Create refresh_osx_dim_customer.sh

---
+++ refresh_osx_dim_customer.sh
@@ -0,0 +1,21 @@
+getDateTime() {
+   # PUT THE CURRENT DATE TIME IN YYYY-MM-DD HH:MM:SS FORMAT AND HOLD IT INSIDE VARIABLE - datetime
+   printf -v datetime '%(%Y-%m-%d %H:%M:%S)T\n' -1
+   echo $datetime
+}
+
+echo $(getDateTime) 'OSX CUSTOMER DIMENSION SCRIPT STARTED' >> $logFilePath
+sh spark-submit \
+--class load_dimension.load_osx_customer \
+--master yarn \
+--deploy-mode cluster \
+--driver-memory 20G \
+--num-executors 10 \
+--executor-memory 5G \
+--executor-cores 3 \
+--conf spark.dynamicAllocation.minExecutors=5 \
+--conf spark.dynamicAllocation.maxExecutors=10 \
+--conf spark.yarn.maxAppAttempts=1 \
+/home/o2072/jars/execute_sumx_load_dim.jar
+
+echo $(getDateTime) 'OSX CUSTOMER DIMENSION SCRIPT COMPLETED' >> $logFilePath
